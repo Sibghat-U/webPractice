04bb5269b454d6e8868b14519070f06191515283
Sibghat Ullah <sibghatwaraich@gmail.com>
2025-10-07 23:42:37 +0500

    update

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-function reverseString() {
-let input=document.querySelector("input");
-let string=input.value,output="";
-for(let i=string.length-1;i>=0;i--){
-  output=output+ string[i];
-}
+let count=0;
+const el=document.createElement("span");
 const p=document.querySelector("p");
-p.innerHTML=output;
+let input=document.querySelector("input");
+
+const body=document.querySelector("body");
+body.appendChild(el);
+
+function reverseString() {
+  p.innerHTML="";
+  let output="";
+  count++;
+  let string=input.value;
+
+for (let i = string.length - 1; i >= 0; i--) {
+  output = output + string[i];
+}
+p.innerHTML=p.innerHTML+output;
+el.innerHTML="Count: "+ count;
+
 }
